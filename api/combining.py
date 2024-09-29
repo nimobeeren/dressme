eb029951a6c1c0534d7a4fb57232a321d2586b78
Nimo Beeren <nimobeeren@gmail.com>
2024-09-29 16:32:02 +0200

    combine garments in api

---
+++ api/combining.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+
+def refine_mask(human, garment_on_human, vton_mask, threshold=1000):
+    # compute the difference between the two images
+    mask = Image.new("L", human.size)
+    for x in range(human.size[0]):
+        for y in range(human.size[1]):
+            r1, g1, b1 = human.getpixel((x, y))
+            r2, g2, b2 = garment_on_human.getpixel((x, y))
+            if ((r2 - r1) ** 2 + (g2 - g1) ** 2 + (b2 - b1) ** 2) > threshold:
+                mask.putpixel((x, y), 255)
+
+    # intersect mask with vton mask
+    for x in range(human.size[0]):
+        for y in range(human.size[1]):
+            if vton_mask.getpixel((x, y)) == 0:
+                mask.putpixel((x, y), 0)
+
+    return mask
+
+
+def combine_garments(human, result_top, result_bottom, mask_top, mask_bottom):
+    result = Image.new("RGB", human.size)
+    refined_mask = refine_mask(human, result_top, mask_top)
+    result.paste(result_bottom, (0, 0))
+    result.paste(result_top, (0, 0), refined_mask)
+    return result
--- api/main.py
+++ api/main.py
@@ -1,8 +1,34 @@
+import io
+
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
+from PIL import Image
+
+from .combining import combine_garments
 
 app = FastAPI()
 
+
 @app.get("/outfit.jpg")
 def get_outfit(top: str, bottom: str):
-    return FileResponse(f'../images/results/multi/{top}_{bottom}.jpg', media_type='image/jpeg')
+    human = Image.open("../images/humans/model.jpg")
+    print(human.format, human.size, human.mode)
+
+    mask_bottom = Image.open("../images/masks/bottom.jpg")
+    print(mask_bottom.format, mask_bottom.size, mask_bottom.mode)
+
+    mask_top = Image.open("../images/masks/top.jpg")
+    print(mask_top.format, mask_top.size, mask_top.mode)
+
+    result_bottom = Image.open(f"../images/results/single/{bottom}.jpg")
+    print(result_bottom.format, result_bottom.size, result_bottom.mode)
+
+    result_top = Image.open(f"../images/results/single/{top}.jpg")
+    print(result_top.format, result_top.size, result_top.mode)
+
+    im = combine_garments(human, result_top, result_bottom, mask_top, mask_bottom)
+
+    img_byte_arr = io.BytesIO()
+    im.save(img_byte_arr, format="JPEG")
+    img_byte_arr.seek(0)
+    return StreamingResponse(img_byte_arr, media_type="image/jpeg")

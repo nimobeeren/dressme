bd55fb010da160d463ccb91bb568ad202a40283c
Nimo Beeren <nimobeeren@gmail.com>
2024-11-29 23:32:01 +0100

    add category tabs

--- client/src/home.tsx
+++ client/src/home.tsx
@@ -1,6 +1,8 @@
+import * as RadioGroup from "@radix-ui/react-radio-group";
 import { CircleAlertIcon, LoaderCircleIcon } from "lucide-react";
 import { useState } from "react";
 import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 import { useWearables, type Wearable } from "./hooks";
 
 export function Home() {
@@ -27,24 +29,70 @@ function OutfitPicker({ wearables }: { wearables: Wearable[] }) {
   const tops = wearables.filter((wearable) => wearable.category === "upper_body");
   const bottoms = wearables.filter((wearable) => wearable.category === "lower_body");
 
-  const [top, setTop] = useState(tops[0]);
-  const [bottom, setBottom] = useState(bottoms[0]);
+  const [topId, setTopId] = useState(tops[0].id);
+  const [bottomId, setBottomId] = useState(bottoms[0].id);
 
   return (
     <div className="flex h-screen items-center justify-center gap-16">
-      <img src={`/images/outfit?top_id=${top.id}&bottom_id=${bottom.id}`} className="h-full" />
-      <div className="grid h-[800px] grid-cols-2 gap-2 overflow-y-auto [scrollbar-gutter:stable]">
-        {tops.map((top) => {
-          return (
-            <img
-              key={top.id}
-              src={top.wearable_image_url}
-              onClick={() => setTop(top)}
-              className="aspect-3/4 w-64 rounded-xl object-cover"
-            />
-          );
-        })}
-      </div>
+      <img src={`/images/outfit?top_id=${topId}&bottom_id=${bottomId}`} className="h-full" />
+      <form className="h-full max-h-[75%]">
+        <Tabs defaultValue="tops" className="flex h-full flex-col items-start gap-2">
+          <TabsList className="shrink-0">
+            <TabsTrigger value="tops">Tops</TabsTrigger>
+            <TabsTrigger value="bottoms">Bottoms</TabsTrigger>
+          </TabsList>
+          <div className="grow overflow-y-auto [scrollbar-gutter:stable]">
+            <TabsContent value="tops">
+              <WearableList
+                value={topId}
+                onValueChange={(value) => setTopId(value)}
+                wearables={tops}
+              />
+            </TabsContent>
+            <TabsContent value="bottoms">
+              <WearableList
+                value={bottomId}
+                onValueChange={(value) => setBottomId(value)}
+                wearables={bottoms}
+              />
+            </TabsContent>
+          </div>
+        </Tabs>
+      </form>
     </div>
+  );
+}
+
+function WearableList({
+  value,
+  onValueChange,
+  wearables,
+}: {
+  value: string;
+  onValueChange: (value: string) => void;
+  wearables: Wearable[];
+}) {
+  return (
+    <RadioGroup.Root
+      value={value}
+      onValueChange={onValueChange}
+      className="grid grid-cols-2 content-start gap-4"
+    >
+      {wearables.map((wearable) => {
+        return (
+          <RadioGroup.Item
+            key={wearable.id}
+            value={wearable.id}
+            className="overflow-hidden rounded-xl transition-all focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-ring"
+          >
+            <img
+              key={wearable.id}
+              src={wearable.wearable_image_url}
+              className="aspect-3/4 w-48 object-cover"
+            />
+          </RadioGroup.Item>
+        );
+      })}
+    </RadioGroup.Root>
   );
 }

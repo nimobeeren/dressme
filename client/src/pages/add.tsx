5f1fd9505b018ddf03759a76c442dad72ac33f6f
Nimo Beeren <nimobeeren@gmail.com>
2025-01-27 20:13:22 +0100

    wip: implement form (radio group broken)

--- client/src/pages/add.tsx
+++ client/src/pages/add.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useFieldArray, useForm } from "react-hook-form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useFieldArray, useForm, type SubmitHandler } from "react-hook-form";
 
 type ClothingItem = {
-  id: string;
+  // id: string;
   file: File;
   preview: string;
   category?: "top" | "bottom";
@@ -16,15 +18,19 @@ type FormData = {
 };
 
 export function Add() {
-  const { register, control, handleSubmit } = useForm<FormData>({
+  // TODO
+  // const { mutate: addWearable } = useAddWearable();
+
+  const { register, control, handleSubmit, watch } = useForm<FormData>({
     defaultValues: {
       // TODO: revert
       // wearables: []
       wearables: [
         {
-          id: Math.random().toString(36).slice(2, 9),
+          // id: Math.random().toString(36).slice(2, 9),
           file: new File([], "test"),
           preview: "http://localhost:8000/images/wearables/72f043bf-63f4-4719-aa3b-d5f0aafd587e",
+          description: "",
         },
       ],
     },
@@ -35,42 +41,64 @@ export function Add() {
     name: "wearables",
   });
 
-  const handleFiles = (files: FileList) => {
-    Array.from(files).forEach((file) => {
-      append({
-        id: Math.random().toString(36).slice(2, 9),
-        file,
-        preview: URL.createObjectURL(file),
-      });
-    });
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (e.target.files && e.target.files[0]) {
-      handleFiles(e.target.files);
+      Array.from(e.target.files).forEach((file) => {
+        append({
+          file,
+          preview: URL.createObjectURL(file),
+        });
+      });
     }
   };
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    console.log("submitting", data);
+  };
+
+  watch((data) => console.log("watch", JSON.stringify(data, null, 2)));
 
   return (
     <div className="container mx-auto py-10">
       <h1 className="mb-6 text-3xl font-bold">Add Clothing Items</h1>
-      <form className="space-y-8">
-        <Input type="file" multiple onChange={handleChange} {...register} />
+      <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
+        {/* This field is not registered to the form, instead the value is captured in `onChange`
+        and set with `useFieldArray` */}
+        <Input type="file" multiple onChange={onFileInputChange} />
 
-        {fields.map((wearable) => (
+        {fields.map((wearable, index) => (
           <Card key={wearable.id} className="flex flex-row">
             <img src={wearable.preview} className="aspect-3/4 h-64 object-cover" />
-            <div className="p-8">
+            <div className="space-y-8 p-8">
               <div className="space-y-2">
-                {/* LEFT HERE */}
-                {/* TODO: make this a radio group (or even better: use the same UI as tabs) */}
-                <Label htmlFor={`wearable:${wearable.id}:category`}>Category</Label>
-                <Input id={`wearable:${wearable.id}:category`} {...register} />
+                <Label htmlFor={`wearables.${index}.category`}>Category</Label>
+                <RadioGroup
+                  className="flex space-x-4"
+                  {...register(`wearables.${index}.category`, { required: true })}
+                >
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="top" id={`wearables.${index}.category.top`} />
+                    <Label htmlFor={`wearables.${index}.category.top`}>Top</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="top" id={`wearables.${index}.category.bottom`} />
+                    <Label htmlFor={`wearables.${index}.category.bottom`}>Bottom</Label>
+                  </div>
+                </RadioGroup>
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor={`wearables.${index}.description`}>Description</Label>
+                <Input
+                  {...register(`wearables.${index}.description`)}
+                  id={`wearables.${index}.description`}
+                  type="text"
+                />
               </div>
             </div>
           </Card>
         ))}
+        <Button type="submit">Add</Button>
       </form>
     </div>
   );

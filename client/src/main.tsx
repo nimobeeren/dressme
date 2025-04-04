6a6be51a9c7c08097beeab7addfb88fecb1b2911
Nimo Beeren <nimobeeren@gmail.com>
2025-04-04 21:44:26 +0200

    move app to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,54 @@
-import { StrictMode } from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/hooks/use-toast";
+import { Home } from "@/pages/home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode, useMemo } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./app";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AuthProvider } from "./components/auth-provider";
 import "./index.css";
+import { NotFound } from "./pages/404";
+import { Add } from "./pages/add";
+import { Login } from "./pages/login";
+import { Profile } from "./pages/profile";
+
+const queryClient = new QueryClient();
+
+function App() {
+  const { toast } = useToast();
+
+  useMemo(() => {
+    queryClient.setDefaultOptions({
+      mutations: {
+        onError: (error) => {
+          console.error(error);
+          toast({
+            title: "Oops, something went wrong!",
+            description: `Computer says: '${error.message}'`,
+            variant: "destructive",
+          });
+        },
+      },
+    });
+  }, [toast]);
+
+  return (
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+      </QueryClientProvider>
+    </AuthProvider>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

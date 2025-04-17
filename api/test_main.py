b88839459d9bdf4b204f0775c2a19057c993bec6
Nimo Beeren <nimobeeren@gmail.com>
2025-04-17 19:21:43 +0200

    add a few tests

--- api/test_main.py
+++ api/test_main.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from fastapi.testclient import TestClient
 import pytest
 from sqlmodel import Session, create_engine, SQLModel
@@ -22,7 +23,7 @@ def session_fixture():
 def client_fixture(session: Session):
     def get_session_override():
         return session
-    
+
     def verify_token_override():
         return {"sub": "auth0|123"}
 
@@ -34,7 +35,7 @@ def client_fixture(session: Session):
     app.dependency_overrides.clear()
 
 
-def test_get_users(session: Session, client: TestClient):
+def test_get_users_success(session: Session, client: TestClient):
     avatar_image_1 = db.AvatarImage(image_data=b"")
     user_1 = db.User(auth0_user_id="auth0|123", avatar_image=avatar_image_1)
     session.add(avatar_image_1)
@@ -48,10 +49,8 @@ def test_get_users(session: Session, client: TestClient):
     session.commit()
 
     response = client.get("/users")
-    app.dependency_overrides.clear()
-    data = response.json()
-
     assert response.status_code == 200
+    data = response.json()
 
     assert len(data) == 2
     assert data[0]["id"] == str(user_1.id)
@@ -60,3 +59,49 @@ def test_get_users(session: Session, client: TestClient):
     assert data[1]["id"] == str(user_2.id)
     assert data[1]["auth0_user_id"] == user_2.auth0_user_id
     assert data[1]["avatar_image_url"] == f"/images/avatars/{user_2.avatar_image_id}"
+
+
+def test_get_wearables_success(session: Session, client: TestClient):
+    wearable_image_1 = db.WearableImage(image_data=b"")
+    wearable_1 = db.Wearable(
+        wearable_image=wearable_image_1, category="top", description="test top"
+    )
+    session.add(wearable_image_1)
+    session.add(wearable_1)
+
+    wearable_image_2 = db.WearableImage(image_data=b"")
+    wearable_2 = db.Wearable(wearable_image=wearable_image_2, category="bottom")
+    session.add(wearable_image_2)
+    session.add(wearable_2)
+
+    session.commit()
+
+    response = client.get("/wearables")
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 2
+    assert data[0]["id"] == str(wearable_1.id)
+    assert data[0]["category"] == wearable_1.category
+    assert data[0]["description"] == wearable_1.description
+    assert data[1]["id"] == str(wearable_2.id)
+    assert data[1]["category"] == wearable_2.category
+    assert data[1]["description"] == wearable_2.description
+
+
+def test_get_wearable_image_success(session: Session, client: TestClient):
+    wearable_image = db.WearableImage(
+        image_data=b'RIFF.\x00\x00\x00WEBPVP8 "\x00\x00\x000\x01\x00\x9d\x01*\n\x00\n\x00\x01@&%\xa4\x00\x03p\x00\xfe\xfa0L f}\x19l\xc5\xd6+\x80\x00\x00'
+    )
+    session.add(wearable_image)
+    session.commit()
+
+    response = client.get(f"/images/wearables/{wearable_image.id}")
+    assert response.status_code == 200
+    assert response.content == wearable_image.image_data
+
+
+def test_get_wearable_image_not_found(session: Session, client: TestClient):
+    response = client.get(f"/images/wearables/{uuid4()}")
+    assert response.status_code == 404
+

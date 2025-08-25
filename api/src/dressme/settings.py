b38411d509b3a1cce27b0395d584b28ab5bfd588
Nimo Beeren <nimobeeren@gmail.com>
2025-08-25 22:05:02 +0200

    switch to psycopg 3

--- api/src/dressme/settings.py
+++ api/src/dressme/settings.py
@@ -11,7 +11,8 @@ class Settings(BaseSettings):
     AUTH0_SEED_USER_ID: str | None = None
     """Auth0 User ID of the user who should own the data added during database seeding."""
     DATABASE_URL: str
-    """PostgreSQL connection string."""
+    """PostgreSQL connection string. We use the psycopg 3 adapter, which requires that the
+    protocol of the URL is `postgresql+psycopg:`."""
     REPLICATE_API_TOKEN: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")

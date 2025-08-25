41ecabc121dc65d6b9c82bfc36132652d474c41d
Nimo Beeren <nimobeeren@gmail.com>
2025-08-25 21:32:06 +0200

    switch to postgres

--- api/src/dressme/settings.py
+++ api/src/dressme/settings.py
@@ -4,13 +4,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    REPLICATE_API_TOKEN: str
     AUTH0_ALGORITHMS: str
     AUTH0_API_AUDIENCE: str
     AUTH0_DOMAIN: str
     AUTH0_ISSUER: str
     AUTH0_SEED_USER_ID: str | None = None
     """Auth0 User ID of the user who should own the data added during database seeding."""
+    DATABASE_URL: str
+    """PostgreSQL connection string."""
+    REPLICATE_API_TOKEN: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 

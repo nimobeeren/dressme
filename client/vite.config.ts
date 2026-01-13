d07b949d3dbe01e77e0ba25c298cfd9e13699d59
Nimo Beeren <nimobeeren@gmail.com>
2026-01-13 21:11:25 +0100

    go back to port 5173 for client since workaround is no longer necessary

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,20 +5,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
-  server: {
-    // Must be the same as the port the API container runs on
-    // Related issues:
-    // - https://github.com/cloudflare/containers/issues/65
-    // - https://github.com/cloudflare/workers-sdk/issues/9793
-    // - https://github.com/cloudflare/workers-sdk/issues/10391
-    // All are now closed but now we're getting a new error when setting this back to 5173:
-    // Error checking if container is ready: The operation was aborted
-    // Container error: [Error: Container exited with unexpected exit code: 94216] {
-    //   exitCode: 94216
-    // }
-    // ...repeating with different error codes each time
-    port: 8000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
